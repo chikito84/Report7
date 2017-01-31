28dd38d47ee159a3c24fe1ed75e27adfaf313dd4
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2017-01-31 17:13:40 +0900

    maked function of MAIN.

--- src/main/java/jp/ac/uryukyu/ie/e165728/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165728/Main.java
@@ -1,7 +1,17 @@
 package jp.ac.uryukyu.ie.e165728;
 
-/**
- * Created by e165728 on 2017/01/31.
- */
 public class Main {
+    public static void main(String[] args){
+        Play play = new Play();
+        int[] a = play.getMt();
+        int b = a[0]^a[1]^a[2];
+        System.out.println("山は（" + a[0] + "," + a[1] + "," + a[2] + ")です。");
+
+        if(b == 0){
+            play.Player();
+        }else{
+            play.Computer();
+        }
+        System.out.println("COMの勝利です。");
+    }
 }
