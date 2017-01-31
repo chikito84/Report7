40736fcbb468932492fa10ea556324e42b1d8045
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2017-01-31 17:07:20 +0900

    maked function of COMPUTER.

--- src/main/java/jp/ac/uryukyu/ie/e165728/Play.java
+++ src/main/java/jp/ac/uryukyu/ie/e165728/Play.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Play {
     Field field = new Field();
 
-    private int stone, num;
+    private int stone, num, a, b, ex, k, i, n, kk;
     private int[] Mt = {field.getVal1(), field.getVal2(), field.getVal3()};
 
     public void Player(){
@@ -45,6 +45,42 @@ public class Play {
     }
 
     public void Computer(){
+        a = (int) (Math.random() * 9) + 1;
+        b = (int) (Math.random() * 3);
 
+        while (Mt[0] + Mt[1] + Mt[2] != 0) {
+            if (Mt[0] != 0 && Mt[1] == 0 && Mt[2] == 0) {
+                System.out.println("COMは1番目の山から石を" + Mt[0] + "個取った。");
+                Mt[0] = 0;
+                System.out.println("山は（" + Mt[0] + "," + Mt[1] + "," + Mt[2] + ")です。");
+
+            } else if (Mt[0] == 0 && Mt[1] != 0 && Mt[2] == 0) {
+                System.out.println("COMは1番目の山から石を" + Mt[1] + "個取った。");
+                Mt[1] = 0;
+                System.out.println("山は（" + Mt[0] + "," + Mt[1] + "," + Mt[2] + ")です。");
+
+            } else if (Mt[0] == 0 && Mt[1] == 0 && Mt[2] != 0) {
+                System.out.println("COMは1番目の山から石を" + Mt[2] + "個取った。");
+                Mt[2] = 0;
+                System.out.println("山は（" + Mt[0] + "," + Mt[1] + "," + Mt[2] + ")です。");
+            } else{
+                ex = Mt[0]^Mt[1]^Mt[2];
+                if (ex != 0) {
+                    k = 0;
+                    for (i = 0; i < 3; i++) {
+                        k = Mt[i] ^ ex;
+                        if (Mt[i] > k) {
+                            n = i;
+                            break;
+                        }
+                    }
+                    kk = Mt[n] - k;
+                    Mt[n] = k;
+                    System.out.println("COMは" + (n + 1) + "番目の山から" + kk + "個の石を取った。");
+                    System.out.println("山は（" + Mt[0] + "," + Mt[1] + "," + Mt[2] + ")です。");
+                }
+                Player();
+            }
+        }
     }
 }
